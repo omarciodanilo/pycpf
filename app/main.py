53689c05605d33300627cfedbeb1237cb4ac61e5
Márcio Almeida <>
2025-11-02 13:25:29 -0300

    Adição de gerar múltiplos CPFs

--- app/main.py
+++ app/main.py
@@ -16,31 +16,42 @@ def main():
 
     if escolha in ("1", "2"):
         limpa_tela()
+        qtd = 1
+        dict_cpf = {}
+        item = 0
         if escolha == "1":
             print("1 - GERAR CPF\n")
-            cpf = gera_cpf()
-            while cpf.count(cpf[0]) == 11:
+            qtd = int(input("Insira a quantidade de CPFs para gerar: "))
+
+            for item in range(qtd):
                 cpf = gera_cpf()
-            situacao = "válido"
-            uf = verifica_uf(cpf[8])
+                while cpf.count(cpf[0]) == 11:
+                    cpf = gera_cpf()
+                situacao = "válido"
+                uf = verifica_uf(cpf[8])
+                dict_cpf[item] = [cpf,situacao,uf]
         else:
             print("2 - VALIDAR CPF\n")
             cpf = input("Insira o CPF para validação (apenas números): ")
-
             print()
+
             if verifica_cpf(cpf):
                 primeiro_dv = calcula_dv(cpf, 10)
                 segundo_dv = calcula_dv(cpf, 11)
                 situacao = compara_dv(cpf, primeiro_dv, segundo_dv)
                 if situacao == "válido":
                     uf = verifica_uf(cpf[8])
+                else:
+                    uf = "não se aplica"
             else:
                 situacao = "inválido"
+                uf = "não se aplica"
+            dict_cpf[item] = [cpf,situacao,uf]
 
-        print(f"CPF      : { converte_cpf(cpf) }")
-        print(f"SITUAÇÃO : { situacao }")
-        if situacao == "válido":
-            print(f"UF       : { uf }")
+        for i in range(qtd):
+            print(f"CPF      : { converte_cpf(dict_cpf[item][0]) }")
+            print(f"SITUAÇÂO : { dict_cpf[item][1] }")
+            print(f"UF       : { dict_cpf[item][2] }\n")
     else:
         sys.exit()
 
