ef137d5c65904c632e5ca0d7562d04b05edf95cc
Márcio Almeida <>
2025-11-22 12:01:24 -0300

    Usar GET+POST, validar None, limitar input text
    - (main.py) Usar métodos GET e POST para as funções gerar e validar;
    - (main.py) Verificar se objeto é str ao abrir URL /validar sem adição
      de CPF (/validar?cpf=STRING) para evitar erro 500 (object NoneType);
    - (index.html) Limitar input text para, no máximo, 11 caracteres;
    - (style.css) Limitar input text para largura de 200px;

--- app/main.py
+++ app/main.py
@@ -4,93 +4,46 @@ from flask import Flask, render_template, request
 # import dos módulos do projeto
 from pycpf import converte_cpf, gera_cpf, verifica_cpf, calcula_dv, compara_dv, verifica_uf
 
-"""
-def main():
-
-    #Solicita informações ao usuário e retorna saída na tela.
-
-    print("GERADOR E VALIDADOR DE CPF")
-    print("\nO que você deseja:\n  1. Gerar um CPF válido\n  2. Validar um CPF\n  OBS: para sair, pressione qualquer outra tecla")
-    escolha = input("\nEscolha: ")
-
-    if escolha in ("1", "2"):
-        qtd = 1
-        dict_cpf = {}
-        item = 0
-        if escolha == "1":
-            print("1 - GERAR CPF\n")
-            qtd = int(input("Insira a quantidade de CPFs para gerar: "))
-
-            for item in range(qtd):
-                cpf = gera_cpf()
-                while cpf.count(cpf[0]) == 11:
-                    cpf = gera_cpf()
-                situacao = "válido"
-                uf = verifica_uf(cpf[8])
-                dict_cpf[item] = [cpf,situacao,uf]
-        else:
-            print("2 - VALIDAR CPF\n")
-            cpf = input("Insira o CPF para validação (apenas números): ")
-            print()
-
-            if verifica_cpf(cpf):
-                primeiro_dv = calcula_dv(cpf, 10)
-                segundo_dv = calcula_dv(cpf, 11)
-                situacao = compara_dv(cpf, primeiro_dv, segundo_dv)
-                if situacao == "válido":
-                    uf = verifica_uf(cpf[8])
-                else:
-                    uf = "não se aplica"
-            else:
-                situacao = "inválido"
-                uf = "não se aplica"
-            dict_cpf[item] = [cpf,situacao,uf]
-
-        print()
-        for i in range(qtd):
-            print(f"CPF      : { converte_cpf(dict_cpf[item][0]) }")
-            print(f"SITUAÇÂO : { dict_cpf[item][1] }")
-            print(f"UF       : { dict_cpf[item][2] }\n")
-    else:
-        sys.exit()
-"""
-
 app = Flask(__name__)
 
-@app.route("/", methods=['GET','POST'])
+@app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
 
-@app.route("/gerar", methods=['POST'])
+@app.route('/gerar', methods=['GET', 'POST'])
 def gerar():
 	lista_cpf = []
 	cpf = gera_cpf()
-	if request.method == "POST":
-		while cpf.count(cpf[0]) == 11:
-			cpf = gera_cpf()
-		uf = verifica_uf(cpf[8])
-		cpf = converte_cpf(cpf)
-		lista_cpf = [cpf, uf]
+	while cpf.count(cpf[0]) == 11:
+		cpf = gera_cpf()
+	uf = verifica_uf(cpf[8])
+	cpf = converte_cpf(cpf)
+	lista_cpf = [cpf, uf]
 	return lista_cpf
 
-@app.route("/validar", methods=['POST'])
+@app.route('/validar', methods=['GET', 'POST'])
 def validar():
 	lista_cpf = []
-	if request.method == "POST":
-		cpf = request.form.get("cpf")
-		if verifica_cpf(cpf):
-			primeiro_dv = calcula_dv(cpf, 10)
-			segundo_dv = calcula_dv(cpf, 11)
-			situacao = compara_dv(cpf, primeiro_dv, segundo_dv)
-			if situacao == "correto":
-				uf = verifica_uf(cpf[8])
-			else:
-				uf = ""
+	cpf = ''
+	
+	if request.method == 'POST':
+		cpf = request.form.get('cpf')
+	elif type(request.args.get('cpf')) is str:
+		cpf = request.args.get('cpf')
+		
+	if verifica_cpf(cpf):
+		primeiro_dv = calcula_dv(cpf, 10)
+		segundo_dv = calcula_dv(cpf, 11)
+		situacao = compara_dv(cpf, primeiro_dv, segundo_dv)
+		if situacao == 'correto':
+			uf = verifica_uf(cpf[8])
 		else:
-			situacao = "incorreto"
-			uf = ""
-		lista_cpf = [cpf, situacao, uf]
+			uf = ''
+	else:
+		situacao = 'incorreto'
+		uf = ''
+	lista_cpf = [cpf, situacao, uf]
 	return lista_cpf
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
