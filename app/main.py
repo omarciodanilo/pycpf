84df689cf74fcc1e762e582a6ed939306ef6a777
Márcio Almeida <>
2026-01-18 15:15:07 -0300

    Comunicação com o PostgreSQL

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 # imports do Python
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, g
+import psycopg2
+import time
 
 # import dos módulos do projeto
 from pycpf import converte_cpf, gera_cpf, verifica_cpf, calcula_dv, compara_dv, verifica_uf
@@ -11,6 +13,44 @@ dict_cpf = {
 	'situacao': '',
 	'uf': '',
 }
+
+# Comunicação com o PostgreSQL
+conexao_db = psycopg2.connect(
+    database="pycpfdb",
+    host="localhost",
+    user="pycpf",
+    password="pycpf",
+    port="5432"
+)
+
+@app.before_request
+def iniciar_timer():
+    g.start_time = time.time()
+
+@app.after_request
+def logs_requisicoes(response):
+    tempo_execucao = time.time() - g.start_time
+
+    metodo_http = request.method.upper()
+    interacao = bool(request.get_data() or request.values)
+    pagina = request.endpoint.upper() if request.endpoint else 'DESCONHECIDO'
+    status_http = response.status_code
+
+    insert_sql = """
+        INSERT INTO logs_requisicoes (metodo_http, interacao, pagina, status_http, tempo_execucao) VALUES (%s, %s, %s, %s, %s);
+    """
+    cursor = conexao_db.cursor()
+    cursor.execute(insert_sql, (
+        metodo_http,
+        interacao,
+        pagina,
+        status_http,
+        tempo_execucao
+        )
+    )
+    conexao_db.commit()
+    cursor.close()
+    return response
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
